a2c2dbe5c336a67f8e48205962c35530e92c02e6
Ubuntu <ubuntu@domU-12-31-39-0C-09-EE.compute-1.internal>
2013-03-21 23:05:38 +0000

    .

--- chef-repo/config/knife.rb
+++ chef-repo/config/knife.rb
@@ -4,7 +4,7 @@ log_location             STDOUT
 node_name                "anthroprose"
 client_key               "#{current_dir}/anthroprose.pem"
 validation_client_name   "anthroprose-validator"
-validation_key           "/home/acorley/.chef/anthroprose-validator.pem"
+validation_key           "/home/ubuntu/.chef/anthroprose-validator.pem"
 chef_server_url          "https://api.opscode.com/organizations/anthroprose"
 cache_type               'BasicFile'
 cache_options( :path => "#{ENV['HOME']}/.chef/checksums" )
