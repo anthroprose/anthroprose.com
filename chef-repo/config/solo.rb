3c7cfc6c4ce5d02da97f95ea3e328b2034150d7d
Alex Corley <acorley@acorley.(none)>
2013-03-21 13:53:45 -0500

    Initial commit.

---
+++ chef-repo/config/solo.rb
@@ -0,0 +1,16 @@
+file_cache_path "/tmp/chef"
+cookbook_path ["/home/ubuntu/anthroprose/chef-repo/cookbooks"]
+role_path nil
+log_level :info
+
+encrypted_data_bag_secret "/tmp/encrypted_data_bag_secret"
+
+data_bag_path "/home/ubuntu/anthroprose/chef-repo/data_bags"
+
+http_proxy nil
+http_proxy_user nil
+http_proxy_pass nil
+https_proxy nil
+https_proxy_user nil
+https_proxy_pass nil
+no_proxy nil
