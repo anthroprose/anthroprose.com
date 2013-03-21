057f90ae8036457ec87fc844e173a0168eb622fb
Alex Corley <acorley@acorley.(none)>
2013-03-21 15:17:16 -0500

    .

--- chef-repo/cookbooks/anthroprose/metadata.rb
+++ chef-repo/cookbooks/anthroprose/metadata.rb
@@ -2,7 +2,6 @@ maintainer       "Alex Corley"
 maintainer_email "acorley@anthroprose.com"
 license          "Apache 2.0"
 description      "Manages anthroprose.com"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
 supports "ubuntu"
