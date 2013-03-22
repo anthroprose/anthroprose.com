7e291fc61633389a04f3cea14700c3a0fc49cc78
Alex Corley <acorley@acorley.(none)>
2013-03-21 22:20:44 -0500

    final for init bootstrap

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-
-curl -L https://www.opscode.com/chef/install.sh | sudo bash
+sudo apt-get update
+sudo apt-get install git ruby1.9.1 ruby1.9.1-dev build-essential
+sudo gem install chef --no-ri --no-rdoc
 git clone https://github.com/anthroprose/anthroprose.com.git ./anthroprose
