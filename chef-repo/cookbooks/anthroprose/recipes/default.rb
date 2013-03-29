a217311114821a3856c339109653e03b7e589d30
Alex Corley <acorley@acorley.(none)>
2013-03-28 22:33:19 -0500

    bootstrapping databag into node attrs

--- chef-repo/cookbooks/anthroprose/recipes/default.rb
+++ chef-repo/cookbooks/anthroprose/recipes/default.rb
@@ -1,15 +1,3 @@
-config = data_bag_item(node['databag'], config)
-
-node['mysql']['server']['server_root_password'] = config["mysql_root_password"]
-node['mysql']['server']['server_debian_password'] = config["mysql_root_password"]
-node['mysql']['server']['server_repl_password'] = config["mysql_root_password"]
-node['wordpress']['db']['password'] = config["mysql_root_password"]
-node['wordpress']['keys']['auth'] = config["wordpress_keys_hash"]
-node['wordpress']['keys']['secure_auth'] = config["wordpress_keys_hash"]
-node['wordpress']['keys']['logged_in'] = config["wordpress_keys_hash"]
-node['wordpress']['keys']['nonce'] = config["wordpress_keys_hash"]
-
-
 Array(node['dependencies']).each do |p|
   package p do
     action :install
