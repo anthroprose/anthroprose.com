bbb6607637697b7d51ca7740e86fbe8ccb491ccb
Alex Corley <acorley@acorley.(none)>
2013-04-06 20:27:43 -0500

    cloudformation!

--- chef-repo/cookbooks/anthroprose/recipes/default.rb
+++ chef-repo/cookbooks/anthroprose/recipes/default.rb
@@ -1,5 +1,5 @@
 execute "hostname" do
-  command "echo #{node['nginx']['default_domain']} >> /etc/hostname;host -F /etc/hostname"
+  command "echo #{node['nginx']['default_domain']} >> /etc/hostname;hostname -F /etc/hostname"
   creates "#{node['tinytinyrss']['dir']}/db.log"
 end
 
